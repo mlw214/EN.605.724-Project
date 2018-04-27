bbceea73722de81680e9e4f512293048f5b41117
Miller Wilt <miller.wilt@gmail.com>
2018-04-27 12:20:43 -0400

    Add Wasserstein loss and learning curve utility

---
+++ sgantech/losses.py
@@ -0,0 +1,7 @@
+
+import keras.backend as K
+
+def wasserstein_loss(y_true, y_pred):
+    return K.mean(y_true * y_pred)
+
+
--- sgantech/utils.py
+++ sgantech/utils.py
@@ -11,3 +11,19 @@ def load_preprocessed_mnist():
     x_test = np.expand_dims(x_test, axis=3)
 
     return (x_train, y_train), (x_test, y_test)
+
+def learning_curve_wrapper(train_test_fn, x_train, y_train, x_test, y_test, start=None, stop=None, step_size=1000):
+    if start is None:
+        start = step_size
+    if stop is None:
+        stop = len(x_train) + step_size
+    
+    train_losses, test_losses, steps = [], [], []
+    for num_examples in np.arange(start, stop, step_size):
+        steps.append(num_examples)
+
+        train_loss, test_loss = train_test_fn(num_examples, x_train, y_train, x_test, y_test)
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+
+    return np.asarray(train_losses), np.asarray(test_losses), np.asarray(steps)
